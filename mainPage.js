0feb30085c9fe6d74566ceea6714a82503074acd
andychen23 <zhizhengchen@zhizhengchens-MacBook-Pro.local>
2016-01-23 21:53:29 -0500

    DinngApp1
    Bruegger’s is working

---
+++ mainPage.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+function init() {
+	var but = document.getElementById("bb");
+
+	var burger = document.getElementById("p4");
+	burger.addEventListener("click", function(){ window.open("Bruegger's.html")});
+}
+
+function call(){
+	alert("aa");
+}
+
+window.addEventListener('load', init);
